8411a70209919761164fdf348d172b491557445e
liuzhichao <liuzhichao_llhc@outlook.com>
2017-03-13 17:25:16 +0800

    Create Project by vue-cli

---
+++ config/prod.env.js
@@ -0,0 +1,6 @@
+var api_config = require('./api_config');
+
+module.exports = {
+  NODE_ENV: '"production"',
+  API_SERVER:api_config.development
+};
---
+++ src/store/getters.js
@@ -0,0 +1,3 @@
+/**
+ * Created by liuzhichao on 2017/3/13.
+ */
